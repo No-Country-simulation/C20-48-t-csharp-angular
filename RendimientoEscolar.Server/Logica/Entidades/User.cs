74c6b1c2103703a919f4fc2c2b79beb32467dad9
MrTorfick <marcososalulu@hotmail.com>
2024-08-24 21:43:55 -0300

    Creacion de clase User

---
+++ RendimientoEscolar.Server/Logica/Entidades/User.cs
@@ -0,0 +1,13 @@
+﻿namespace RendimientoEscolar.Server.Logica.Entidades
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Password { get; set; }
+        public string TipoUsuario { get; set; }//Ver esto
+        public bool Activo { get; set; }
+
+    }
+}
